bfd981441fd89419d131ca4ceb38c5479ab31e2d
Steve <stephentwillis@users.noreply.github.com>
2022-08-22 14:44:45 +0100

    Create additional-events.is

---
+++ F1/schengen-area/additional-events.js
@@ -0,0 +1,14 @@
+{
+    "eventCode": "01_01Abu",
+    "venue": "Abu Dhabi",
+    "date": "01/01/2022",
+    "duration": 3,
+    "type": "Race"
+},
+{
+    "eventCode": "Can06_01",
+    "venue": "Canada",
+    "date": "01/06/2021",
+    "duration": 4,
+    "type": "Test"
+}
